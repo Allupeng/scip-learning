69636e0c43f15ae26bec04c0187cab4c00ce8ae7
allupeng <allupeng417@gmail.com>
2023-02-14 14:01:02 +0800

    finish problem 0、1、2、3

--- python-scip/projects/hog/hog.py
+++ python-scip/projects/hog/hog.py
@@ -22,7 +22,16 @@ def roll_dice(num_rolls, dice=six_sided):
     assert type(num_rolls) == int, 'num_rolls must be an integer.'
     assert num_rolls > 0, 'Must roll at least once.'
     # BEGIN PROBLEM 1
-    "*** YOUR CODE HERE ***"
+    one_once = False
+    res = 0
+    i = 0
+    while i < num_rolls:
+        value = dice()
+        if value == 1:
+            one_once = True
+        res += value
+        i += 1
+    return 1 if one_once else res
     # END PROBLEM 1
 
 
@@ -33,7 +42,13 @@ def tail_points(opponent_score):
 
     """
     # BEGIN PROBLEM 2
-    "*** YOUR CODE HERE ***"
+    # resize opponent_score to 2 bit num
+    # like 1234 -> 23
+    while opponent_score // 100 != 0:
+        opponent_score %= 100
+    tens = opponent_score // 10
+    ones = opponent_score % 10
+    return 2 * abs(tens - ones) + 1
     # END PROBLEM 2
 
 
@@ -50,7 +65,10 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert num_rolls >= 0, 'Cannot roll a negative number of dice in take_turn.'
     assert num_rolls <= 10, 'Cannot roll more than 10 dice.'
     # BEGIN PROBLEM 3
-    "*** YOUR CODE HERE ***"
+    if num_rolls == 0:
+        return tail_points(opponent_score)
+    else:
+        return roll_dice(num_rolls,dice)
     # END PROBLEM 3
 
 
